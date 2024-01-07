a0f2e5791c1a2254c6cc60b963a4e0cd5a3665d9
ever-jr <evertonxdoj@gmail.com>
2024-01-07 11:45:20 -0300

    add: health bar changes

--- src/starter_player/character/damage_test.client.lua
+++ src/starter_player/character/damage_test.client.lua
@@ -1,9 +1,5 @@
 local rs = game:GetService('ReplicatedStorage')
 
-local player = game:GetService('Players').LocalPlayer
-local character = player.Character or player.CharacterAdded:Wait()
-local humanoid = character.Humanoid
-
 -- events
 local inputEvent = game:GetService('UserInputService')
 local onPlayerHit = rs.events.on_player_hit
--- src/starter_player/character/on_character_added.client.lua
+++ src/starter_player/character/on_character_added.client.lua
@@ -5,13 +5,9 @@ local userInterface = require(rs.user_interface)
 -- player related
 local player = game:GetService('Players').LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
-local humanoid = character.Humanoid
 
 -- disable core health gui
 starterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Health, false)
-
--- event handlers
-local healthChanged = humanoid.HealthChanged
 
 -- create interface
 userInterface.new(player)
