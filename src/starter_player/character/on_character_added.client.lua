843b5e74c0d443f08cdf4651ae2de1474ddb9c60
ever-jr <evertonxdoj@gmail.com>
2024-01-21 10:56:24 -0300

    values separated

--- src/starter_player/character/on_character_added.client.lua
+++ src/starter_player/character/on_character_added.client.lua
@@ -4,9 +4,17 @@ local userInterface = require(rs.user_interface)
 
 -- player related
 local player = game:GetService('Players').LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid: Humanoid = character:WaitForChild('Humanoid')
+
+local playerGui = player.PlayerGui
 
 -- disable core health gui
 starterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Health, false)
 
+-- TODO: see if I can return the whole player variable without having too much trouble
 -- create interface
-userInterface.new(player)
+userInterface.new({
+    humanoid = humanoid,
+    playerGui = playerGui,
+})
